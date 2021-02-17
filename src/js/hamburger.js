572dbe89296655d5dbaa066851bc84c51f51c48b
unknown <soy.facuromero@gmail.com>
2021-02-17 06:37:15 -0300

    Hamburger menu

--- src/js/hamburger.js
+++ src/js/hamburger.js
@@ -1,6 +1,14 @@
-const hamburger = document.querySelector('.header__nav--hamburger');
-const nav = document.getElementById('nav-items');
-
+let hamburger = document.querySelector('.header__mobile');
+let links = document.querySelector('.header__menu');
+let burgerOpen = false;
 hamburger.addEventListener('click', () => {
-    nav.classList.toggle('show');
+    if (!burgerOpen) {
+        hamburger.classList.add('open');
+        links.classList.add('open');
+        burgerOpen = true;
+    } else {
+        hamburger.classList.remove('open');
+        links.classList.remove('open');
+        burgerOpen = false;
+    }
 });
